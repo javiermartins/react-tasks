dcd8b830870eba8ce8516cf555df6e1fed537e2a
javiermartins <javiimartinserrano@gmail.com>
2023-04-15 23:01:26 +0200

    Cancel saved in account and edit photo

--- src/pages/account/Account.tsx
+++ src/pages/account/Account.tsx
@@ -1,6 +1,6 @@
-import { faPencil, faSave } from "@fortawesome/free-solid-svg-icons";
+import { faPencil, faSave, faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { toast } from "react-toastify";
 import userImg from "../../assets/img/user.png";
@@ -10,15 +10,46 @@ import "./Account.scss";
 export default function Account() {
   const [user, setUser] = useState(getUser());
   const [editing, setEditing] = useState(false);
+  const inputFile = useRef<HTMLInputElement>(null);
+
+  const cancelSave = (e: any) => {
+    e.preventDefault();
+    setUser(getUser());
+    setEditing(false);
+  };
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
-    setUser({ ...user, [e.target.name]: e.target.value });
     localStorage.setItem("user", JSON.stringify(user));
+    setEditing(false);
     toast.success("Data successfully saved");
   };
 
-  //TODO: add photo
+  const changeForm = (e: any) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const changeImage = (e: any) => {
+    var reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = () => {
+      setUser({ ...user, photo: reader.result as string });
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ ...user, photo: reader.result })
+      );
+    };
+    reader.onerror = function (error) {
+      console.log("Error: ", error);
+    };
+    console.log(user);
+  };
+
+  const selectImage = () => {
+    if (inputFile.current != null) {
+      inputFile.current.click();
+    }
+  };
 
   return (
     <Layout>
@@ -27,20 +58,30 @@ export default function Account() {
           <div className="row justify-content-center">
             <div className="col-12 col-sm-12 col-md-12 col-lg-9 mb-2 text-end">
               {editing ? (
-                <Button
-                  className="btn-submit text-light rounded-button"
-                  variant="primary"
-                  onClick={() => setEditing(false)}
-                >
-                  <FontAwesomeIcon icon={faSave} className="mr-icon" />
-                  Save
-                </Button>
+                <div>
+                  <Button
+                    className="btn-submit text-light rounded-button me-2"
+                    variant="danger"
+                    onClick={(e) => cancelSave(e)}
+                  >
+                    <FontAwesomeIcon icon={faClose} className="mr-icon" />
+                    Cancel
+                  </Button>
+                  <Button
+                    type="submit"
+                    className="btn-submit text-light rounded-button"
+                    variant="primary"
+                    onClick={() => onSubmit}
+                  >
+                    <FontAwesomeIcon icon={faSave} className="mr-icon" />
+                    Save
+                  </Button>
+                </div>
               ) : (
                 <Button
                   className="text-light rounded-button"
                   variant="primary"
                   onClick={() => setEditing(true)}
-                  type="submit"
                 >
                   <FontAwesomeIcon icon={faPencil} className="mr-icon" />
                   Edit
@@ -52,32 +93,53 @@ export default function Account() {
             <div className="col-12 col-sm-12 col-md-12 col-md-10 col-lg-9 mb-4">
               <div className="card card-body bg-secondary text-dark">
                 <div className="row justify-content-center">
-                  <div className="col-sm-auto text-center">
-                    <img
-                      src={userImg}
-                      alt="User image"
-                      className="user-image"
-                    />
+                  <div className="col-sm-auto">
+                    <div
+                      className="container-image"
+                      onClick={() => selectImage()}
+                    >
+                      <img
+                        src={user.photo ? user.photo : userImg}
+                        alt="User image"
+                        className="user-image"
+                      />
+                      <input
+                        type="file"
+                        accept="image/*"
+                        ref={inputFile}
+                        onChange={changeImage}
+                      />
+                      <div className="image-hover">
+                        <FontAwesomeIcon
+                          icon={faPencil}
+                          className="mr-icon fa-xl"
+                        />
+                      </div>
+                    </div>
                   </div>
                   <div className="col">
                     <Form.Group>
                       <Row>
                         <Col xs={12} sm={12} md lg className="mt-2">
+                          <Form.Label className="text-primary">Name</Form.Label>
                           <Form.Control
                             type="text"
-                            placeholder="Nombre"
                             name="name"
-                            defaultValue={user.name}
+                            value={user.name}
                             disabled={!editing}
+                            onChange={changeForm}
                           />
                         </Col>
                         <Col xs={12} sm={12} md lg className="mt-2">
+                          <Form.Label className="text-primary">
+                            Surnames
+                          </Form.Label>
                           <Form.Control
                             type="text"
-                            placeholder="Apellidos"
                             name="surnames"
-                            defaultValue={user.surnames}
+                            value={user.surnames}
                             disabled={!editing}
+                            onChange={changeForm}
                           />
                         </Col>
                       </Row>
@@ -99,6 +161,7 @@ function getUser() {
     id: null,
     name: "",
     surnames: "",
+    photo: "",
   };
 
   if (userLS) {
